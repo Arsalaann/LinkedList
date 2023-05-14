4d67e1666de0da085ca3bf169a64dfa4e07d0a81
Arsalaann <arsalankhanxk@gmail.com>
2023-05-14 10:51:19 +0530

    Second commit

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
     class Program{
         public static void Main(){
             LinkList list=new LinkList(),root=null;
-            root=list.AddNode(56,root);
+            root=list.AddNode(70,root);
             list.AddNode(30,root);
-            list.AddNode(70,root);
+            list.AddNode(56,root);
             list.DisplayList(root);
         }
     }
